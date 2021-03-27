b20ad32998f38032a4f1a9afcc633f05b0565017
George BV <egor.workroom@gmail.com>
2021-03-27 20:59:02 +0300

    обновлен бинаринк и добавлен пример

--- cmd/okdoc/main.go
+++ cmd/okdoc/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"log"
-	"os"
+	"path/filepath"
 
 	"github.com/fader4/okdoc/annotation"
 )
 
-var pathFileF = flag.String("file", "", "Path to file")
+var pathF = flag.String("path", "", "Path to files")
+var outF = flag.String("out", "okdoc_annots.json", "path to the file with the result")
 
 func main() {
 	flag.Parse()
-
-	list, err := annotation.ParseFile(*pathFileF)
+	log.Println("Interested path with files", *pathF)
+	found, err := filepath.Glob(*pathF)
 	if err != nil {
-		log.Fatalln("Failed parse file", err)
+		log.Fatalln(err)
 	}
-	enc := json.NewEncoder(os.Stdout)
+	log.Println("Found interesting files", found)
+	res := &Result{Files: map[string]interface{}{}}
+	for _, file := range found {
+		annots, err := annotation.ParseFile(file)
+		if err != nil {
+			log.Printf("Failed parse file %q: %v\n", file, err)
+			continue
+		}
+		res.Files[file] = annots
+	}
+
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
 	enc.SetIndent(" ", " ")
-	enc.Encode(list)
+	if err := enc.Encode(res); err != nil {
+		log.Fatalln("Failed to encode json", err)
+	}
+	if err := ioutil.WriteFile(*outF, buf.Bytes(), 0777); err != nil {
+		log.Fatalln("Failed to write to file", err)
+	}
+}
+
+type Result struct {
+	Files map[string]interface{} `json:"files"`
 }
---
+++ examples/file1.star
@@ -0,0 +1,14 @@
+load("foo", "bar")
+
+"""
+@MethodArgument(name = "ctx", description = "ctx with fields")
+@MethodArgument(name = "ctx.foo", description = "ctx with fields")
+@MethodArgument(name = "ctx.bar", description = "ctx with fields")
+"""
+def main(Ctx):
+    if False {
+        # @Return("Some case")
+        return True
+    }
+    # @Return("Happy path")
+    return False
